6112b27c38162cbae44f5ebf314dc5db3925f647
Promille23 <migo.coban@gmail.com>
2025-05-04 20:19:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ async def handle(request):
     return web.Response(text="🔥 Ich bin online, Bro!")
 
 app = web.Application()
-app.router.add_get('/', handle)
-app.router.add_post('/', handle)
-app.router.add_route('*', '/{tail:.*}', handle)
+app.router.add_get('/{tail:.*}', handle)  # fängt ALLE GETs ab
+app.router.add_post('/{tail:.*}', handle)  # fängt ALLE POSTs ab
 
 if __name__ == '__main__':
     web.run_app(app, host='0.0.0.0', port=443)
